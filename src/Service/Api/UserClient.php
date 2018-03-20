cdc8d3df61366097e35b0fc8338ff90b5181cdb1
Vojtěch Lacina <MoraviaD1@gmail.com>
2018-03-20 23:09:38 +0100

    Remove hardcoded API server
    - Added constants

--- src/Service/Api/UserClient.php
+++ src/Service/Api/UserClient.php
@@ -11,6 +11,7 @@ use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Psr7\Request;
 use JMS\Serializer\SerializerInterface;
 use Nette\Utils\Json;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class UserClient.
@@ -20,6 +21,8 @@ class UserClient
     const METHOD_POST = \Symfony\Component\HttpFoundation\Request::METHOD_POST;
     const METHOD_GET = \Symfony\Component\HttpFoundation\Request::METHOD_GET;
     const CONFIG_BASE_URI = 'base_uri';
+    const ACTION_CREATE_USER = 'createUser';
+    const ACTION_USER_LIST = 'userList';
 
     /**
      * @var SerializerInterface
@@ -39,14 +42,20 @@ class UserClient
     /**
      * UserClient constructor.
      *
-     * @param bool $sandbox
+     * @param string              $server
+     * @param bool                $sandbox
+     * @param SerializerInterface $serializer
+     * @param RequestStack        $requestStack
      */
-    public function __construct(SerializerInterface $serializer, bool $sandbox = false)
+    public function __construct(string $server = '', $sandbox = false, SerializerInterface $serializer, RequestStack $requestStack)
     {
+        if (empty($server)) {
+            $server = $requestStack->getCurrentRequest()->getSchemeAndHttpHost();
+        }
         $this->serializer = $serializer;
         $this->sandbox = $sandbox;
         $this->client = new Client([
-            self::CONFIG_BASE_URI => 'http://oxy.local/api/v1/',
+            self::CONFIG_BASE_URI => $server.'/api/v1/',
             'timeout' => 2.0,
             'http_errors' => true,
         ]);
@@ -66,7 +75,7 @@ class UserClient
             $userData['role'] = (string) $userData['role'];
         }
 
-        $request = new Request(self::METHOD_POST, $this->client->getConfig(self::CONFIG_BASE_URI).'createUser', [], Json::encode($userData, true));
+        $request = new Request(self::METHOD_POST, $this->client->getConfig(self::CONFIG_BASE_URI).self::ACTION_CREATE_USER, [], Json::encode($userData, true));
 
         try {
             $response = $this->client->send($request);
@@ -89,7 +98,7 @@ class UserClient
      */
     public function userList(int $page = 1): array
     {
-        $request = new Request(self::METHOD_GET, $this->client->getConfig(self::CONFIG_BASE_URI).'userList'.$this->getPagePath($page));
+        $request = new Request(self::METHOD_GET, $this->client->getConfig(self::CONFIG_BASE_URI).self::ACTION_USER_LIST.$this->getPagePath($page));
         try {
             $response = $this->client->send($request);
             $users = [];
